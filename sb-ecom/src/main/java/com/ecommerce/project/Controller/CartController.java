716fa99ce6996d724f7503ad41fa9bd66d5c876b
Aryan753 <baliyan892329@gmail.com>
2025-09-05 10:12:04 +0530

    Adding product to the cart and create a AuthUtil class for handle the utility of the authenticated user

---
+++ sb-ecom/src/main/java/com/ecommerce/project/Controller/CartController.java
@@ -0,0 +1,24 @@
+package com.ecommerce.project.Controller;
+
+import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    Integer quantity){
+        CartDTO cartDTO=cartService.addProductToCart(productId,quantity);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+}
---
+++ sb-ecom/src/main/java/com/ecommerce/project/util/AuthUtil.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.util;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+
+    @Autowired
+     private UserRepository userRepository;
+
+    public String loggedInEmail(){
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        User user=userRepository.findByUserName(authentication.getName()).
+                orElseThrow(()-> new UsernameNotFoundException("User with this username not found!"));
+        return user.getEmail();
+    }
+    public String loggedInUser(){
+        return null;
+    }
+
+}
